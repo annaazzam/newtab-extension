0ea950ef3d1a577907a4d684108e7617cc2a391a
Anna Azzam <anna.a@canva.com>
2020-06-29 19:48:26 +1000

    default image working

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('script');
+
+const defaultImages = [
+  'https://www.cathaven.com.au/assets/Headers/4294f65d12/Home-page-header__FillWzk3MCwyOTVd.jpg',
+];
+
+chrome.storage.local.remove("newtabPicture");
+
+const getExistingPicture = () => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get("newtabPicture", (result) => {
+      resolve(result.newtabPicture);
+    });
+  });
+}
+
+const setImage = (image) => {
+  document.getElementById('background').src = image;
+};
+
+getExistingPicture().then(pic => {
+  const randomNumber = Math.floor(Math.random() * defaultImages.length);
+  const randomImage = defaultImages[randomNumber];
+  setImage(pic || randomImage);
+});
