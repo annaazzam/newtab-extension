da717e6de3f205a9708babd9f8eb116ed15c4eda
Anna Azzam <anna.a@canva.com>
2020-06-29 20:27:15 +1000

    images from unsplash

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
 console.log('script');
 
+//https://unsplash.com/@username
 const defaultImages = [
-  'https://www.cathaven.com.au/assets/Headers/4294f65d12/Home-page-header__FillWzk3MCwyOTVd.jpg',
+  {
+    url: 'https://images.unsplash.com/photo-1593270573246-2dca2f2c1c43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80',
+    credit: 'Joshua Woroniecki',
+    username: '@joshua_j_woroniecki',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1592490578831-4c55bd5b06d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Neil Thomas',
+    username: '@neilmarkthomas',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1515444744559-7be63e1600de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Rahul Bhosale',
+    username: '@rahul_design',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Bailey Zindel',
+    username: '@baileyzindel',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Pietro De Grandi',
+    username: '@peter_mc_greats',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Luca Bravo',
+    username: '@lucabravo'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1511576661531-b34d7da5d0bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Jeremy Bishop',
+    username: '@jeremybishop',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1433838552652-f9a46b332c40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    credit: 'Daniela Cuevas',
+    username: '@danielacuevas',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1524260855046-f743b3cdad07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80',
+    credit: 'Dave Hoefler',
+    username: '@johnwestrock',
+  },
+  {
+    url: 'https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80',
+    credit: 'Ryan Schroeder',
+    username: '@ryanschroeder',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1459213599465-03ab6a4d5931?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80',
+    credit: 'Marcelo Quinan',
+    username: '@marceloquinan',
+  }
 ];
 
 chrome.storage.local.remove("newtabPicture");
@@ -20,6 +75,6 @@ const setImage = (image) => {
 
 getExistingPicture().then(pic => {
   const randomNumber = Math.floor(Math.random() * defaultImages.length);
-  const randomImage = defaultImages[randomNumber];
+  const randomImage = defaultImages[randomNumber].url;
   setImage(pic || randomImage);
 });
